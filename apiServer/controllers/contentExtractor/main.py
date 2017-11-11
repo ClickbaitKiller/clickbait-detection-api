e8676670eb2befe3f939ba607f78895a6b0ac31e
Matthieu Rudelle <matthieu.rudelle@gmail.com>
2017-11-12 00:59:32 +0100

    working header extractor

--- apiServer/controllers/contentExtractor/main.py
+++ apiServer/controllers/contentExtractor/main.py
@@ -1,3 +1,4 @@
+import json
 from html.parser import HTMLParser
 import re
 
@@ -12,7 +13,7 @@ f.close()
 class MyHTMLParser(HTMLParser):
 
     HEADER_PATTERN = '^h\d$'
-    HEADER_ORDER = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8']
+    HEADER_ORDER = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9']
 
     def __init__(self):
         super().__init__()
@@ -54,8 +55,42 @@ class MyHTMLParser(HTMLParser):
 
         def process_structure(struct):
 
+            print('called on ' + struct['type'] + ' ' + struct['content'])
+
             struct_list = struct['list']
-            level = self.HEADER_ORDER.index(struct['type'])
+
+            if len(struct_list) > 0:
+
+                final_list = []
+
+                element = struct_list.pop(0)
+                element_level = self.HEADER_ORDER.index(element['type'])
+
+                temp_list = []
+
+                for el in struct_list:
+
+                    el_level = self.HEADER_ORDER.index(el['type'])
+
+                    if el_level <= element_level:
+                        element['list'] = temp_list
+                        final_list.append(process_structure(element))
+
+                        element = el
+                        element_level = self.HEADER_ORDER.index(element['type'])
+                        temp_list = []
+                    else:
+                        temp_list.append(el)
+
+                element['list'] = temp_list
+                final_list.append(process_structure(element))
+
+                struct['list'] = final_list
+
+            else:
+                struct['list'] = []
+
+            return struct
 
         return process_structure(structure)
 
@@ -65,12 +100,34 @@ def fetch_html(url):
     pass
 
 
-def get_headers(html):
+def get_listicle_headers(html):
+
     html_header_extractor = MyHTMLParser()
     html_header_extractor.feed(html)
 
-    return html_header_extractor.get_list()
+    nested_headers = html_header_extractor.get_nested()
+
+    def rec_longest(headers, longest_list, longest_list_size):
+
+        if len(headers['list']) > longest_list_size:
+            longest_list_size = len(headers['list'])
+            longest_list = headers
+
+        for x in headers['list']:
+            longest_list, longest_list_size = rec_longest(x, longest_list, longest_list_size)
+
+        return longest_list, longest_list_size
+
+    longest_list, longest_list_size = rec_longest(
+        nested_headers, nested_headers, len(nested_headers['list']))
+
+    main_title = longest_list['content']
+    items = list(map(lambda x: x['content'], longest_list['list']))
+
+    print(main_title, longest_list_size, items)
+
+    return nested_headers
 
 if __name__ == '__main__':
     print(html_content)
-    print(get_headers(html_content))
+    print(json.dumps(get_listicle_headers(html_content), indent=2))
