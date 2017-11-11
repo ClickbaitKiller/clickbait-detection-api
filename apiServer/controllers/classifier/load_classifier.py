b36b0a0f440581a517bb3e1c20b8c47fdf604b81
Matthieu Rudelle <matthieu.rudelle@gmail.com>
2017-11-11 20:03:19 +0100

    working api

---
+++ apiServer/controllers/classifier/load_classifier.py
@@ -0,0 +1,15 @@
+import pickle
+
+def load_classifier():
+  classifier, tfv = pickle.load(open('./controllers/classifier/classifier.p', 'rb'))
+
+  def predict(X):
+    return [p[1] for p in classifier.predict_proba(tfv.transform(X))]
+
+  return predict
+
+predict = load_classifier()
+
+X = ['you will never believe what happens next', 'Obama declares war']
+
+print(predict(X))
--- apiServer/controllers/default_controller.py
+++ apiServer/controllers/default_controller.py
@@ -1,6 +1,13 @@
+from controllers.classifier.load_classifier import load_classifier
+
+predict = load_classifier()
 
 def detect_get(parameters) -> str:
-    return 'do some magic!'
+
+    for p in parameters:
+        p['clickbaitiness'] = predict([p['text']])[0]
+        del p['text']
+    return parameters
 
 def summary_get(parameters) -> str:
     return 'do some magic!'
