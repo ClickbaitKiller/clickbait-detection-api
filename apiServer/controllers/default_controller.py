7c7a997c5007bc7b12746b2b478ce5989474c81f
Matthieu Rudelle <matthieu.rudelle@gmail.com>
2017-11-11 20:33:25 +0100

    more fixes

--- apiServer/controllers/default_controller.py
+++ apiServer/controllers/default_controller.py
@@ -2,13 +2,20 @@ from controllers.classifier.load_classifier import load_classifier
 
 predict = load_classifier()
 
-def detect_get(parameters) -> str:
 
-    for p in parameters:
-        p['clickbaitiness'] = predict([p['text']])[0]
-        del p['text']
+def detect_post(parameters) -> str:
 
-    return parameters
+    texts = map(lambda x: x['text'], parameters)
+    res = zip(parameters, predict(texts))
 
-def summary_get(parameters) -> str:
+    def reconstruct(elem):
+        parameter, score = elem
+        return {
+            'id': parameter['id'],
+            'clickbaitiness': score
+        }
+
+    return list(map(reconstruct, res))
+
+def summary_post(parameters) -> str:
     return 'do some magic!'
